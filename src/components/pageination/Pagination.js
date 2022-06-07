bcd0f03556af69cf8521fc57f9f0c2612f5e7e98
perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2022-06-07 19:47:55 +0530

    pagination update

--- src/components/pageination/Pagination.js
+++ src/components/pageination/Pagination.js
@@ -1,14 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Pagination = ({ limit, totalPosts, paginate, pageNumbers }) => {
+const Pagination = ({
+  limit,
+  totalPosts,
+  paginate,
+  pageNumbers,
+  currentPage,
+}) => {
   for (let i = 1; i <= Math.ceil(totalPosts / limit); i++) {
     pageNumbers.push(i)
   }
 
+  if (currentPage === 0 || pageNumbers.length < 2) {
+    return null
+  }
+
+  const onNext = () => {
+    paginate(currentPage => currentPage + 1)
+  }
+
+  const onPrevious = () => {
+    paginate(currentPage => currentPage - 1)
+  }
+
   return (
-    <nav>
+    <nav aria-label="Page navigation ">
       <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#" onClick={onPrevious}>
+            Previous
+          </a>
+        </li>
         {pageNumbers.map(j => (
           <li key={j} className="page-item">
             <a onClick={() => paginate(j)} className="page-link">
@@ -16,6 +39,11 @@ const Pagination = ({ limit, totalPosts, paginate, pageNumbers }) => {
             </a>
           </li>
         ))}
+        <li className="page-item">
+          <a className="page-link" href="#" onClick={onNext}>
+            Next
+          </a>
+        </li>
       </ul>
     </nav>
   )
@@ -26,6 +54,7 @@ Pagination.propTypes = {
   totalPosts: PropTypes.any,
   paginate: PropTypes.any,
   pageNumbers: PropTypes.any,
+  currentPage: PropTypes.any,
 }
 
 export default Pagination
--- src/pages/Contacts/contacts-grid.js
+++ src/pages/Contacts/contacts-grid.js
@@ -24,7 +24,7 @@ const ContactsGrid = props => {
   const [searchText, setSearchText] = useState("")
 
   const [page, setPage] = useState(1)
-  const [limit, setLimit] = useState(32)
+  const [limit, setLimit] = useState(28)
   const [totalPage, setTotalPage] = useState([])
 
   //pagination//
